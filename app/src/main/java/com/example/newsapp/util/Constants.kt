174cd3bc884a2d6c6bf7a37ee12349c427b86c1f
Arni10 <arnycadabra@gmail.com>
2021-05-27 14:50:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "6dddfdb1c48b4831bf9b1b16ac9ee5b6"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
