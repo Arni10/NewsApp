359e4615bf6ae74e36362efcda07665ae8126df4
Arni10 <arnycadabra@gmail.com>
2021-06-02 20:36:38 +0300

    Third commit - search works (kinda) and pagination added

--- app/src/main/java/com/example/newsapp/util/Constants.kt
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
         const val API_KEY = "6dddfdb1c48b4831bf9b1b16ac9ee5b6"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
